edbbf30e74e9f18d01c9855fceeba18825621d8b
petelouise <programmetress@petelouise.beauty>
2024-06-01 00:44:40 -0500

    Refactored findSnippet and updateSplit functions for better readability and consistency.

--- snippetUtils.ts
+++ snippetUtils.ts
@@ -1,32 +1,40 @@
-export function findSnippet(selectedText: string, snippets: string[], isRegex: boolean): string {
-	let newStr = "";
+export function findSnippet(
+	selectedText: string,
+	snippets: string[],
+	isRegex: boolean
+): string {
+	let newStr = ""
 	for (const snippet of snippets) {
-		const [pattern, replacement] = snippet.split(" : ");
+		const [pattern, replacement] = snippet.split(" : ")
 		if (isRegex) {
-			const regex = new RegExp(pattern);
+			const regex = new RegExp(pattern)
 			if (regex.test(selectedText)) {
-				newStr = selectedText.replace(regex, replacement);
-				break;
+				newStr = selectedText.replace(regex, replacement)
+				break
 			}
 		} else {
 			if (selectedText === pattern) {
-				newStr = replacement;
-				break;
+				newStr = replacement
+				break
 			}
 		}
 	}
-	return newStr;
+	return newStr
 }
-export function UpdateSplit(newlineSymbol: string, snippets_file: string, isRegex: boolean) {
-	let nlSymb = newlineSymbol;
-	nlSymb = nlSymb.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
-	const rg = "(?<!" + nlSymb + ")\\n";
-	const regex = new RegExp(rg);
-	let splited = snippets_file.split(regex);
-	splited = splited.filter((item) => item);
+export function updateSplit(
+	newlineSymbol: string,
+	snippets_file: string,
+	isRegex: boolean
+) {
+	let nlSymb = newlineSymbol
+	nlSymb = nlSymb.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&")
+	const rg = "(?<!" + nlSymb + ")\\n"
+	const regex = new RegExp(rg)
+	let splited = snippets_file.split(regex)
+	splited = splited.filter((item) => item)
 
 	return splited.map((snippet) => {
-		const [pattern, replacement] = snippet.split(" : ");
-		return { pattern, replacement, isRegex };
-	});
+		const [pattern, replacement] = snippet.split(" : ")
+		return { pattern, replacement, isRegex }
+	})
 }
